43a25339fd46c2c76d81ef4eb25eefb8343739ba
JosuaGraf <grafjo@student.ethz.ch>
2020-06-22 16:15:15 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	main/filter_data.py
    #	main/format_data.py
    #	main/gui.ipynb

--- main/config.py
+++ main/config.py
@@ -1,4 +1,10 @@
+'''
 #load data (change if "data" folder is not in the same folder as this file)
 path_to_data = '/Volumes/Drive/ETH/Neural_Systems/b8p2male-b10o15female_aligned'
 data_files_path = '/Volumes/Drive/ETH/Neural_Systems/birdsong_project/main'
 output = '/Volumes/Drive/ETH/Neural_Systems/b8p2male-b10o15female_aligned/filtered'
+'''
+#load data (change if "data" folder is not in the same folder as this file)
+path_to_data = 'C:/Users/Josua Graf/polybox/Neural Systems/b8p2male-b10o15female_aligned(1)/b8p2male-b10o15female_aligned'
+data_files_path = 'C:\\Users\\Josua Graf\\PycharmProjects\\birdsong_project\\main'
+output = 'C:/Users/Josua Graf/PycharmProjects/birdsong_project/main'
